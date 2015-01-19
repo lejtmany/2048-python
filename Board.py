5a34c7f55e090bf2f3723f7696f2b8fffdc4dbde
lejtmany <lejtmany@3eaf17b4-47c3-2875-94e7-d4e5b387a752>
2015-01-19 04:43:05 +0000

    added get_random_value, set_empty_square , move get_square still work in progress

--- Board.py
+++ Board.py
@@ -1,18 +1,36 @@
 __author__ = 'Binary Ninja'
-import BoardSquare
+from BoardSquare import *
+import random
 class Board:
     height = 4
     width = 4
     __squares = []
 
     def __init__(self):
-        Board.__generate_board_squares()
+        self.__generate_board_squares()
 
     def __generate_board_squares(self):
-        for x in range(0,3):
-            for y in range(0,3):
-                Board.__squares.append(BoardSquare(BoardSquare.empty_value,x,y))
+        for x in range(0,4):
+            for y in range(0,4):
+                self.__squares.append(BoardSquare(BoardSquare.empty_value,x,y))
 
     def pop_random_square(self):
-        empty_squares = []
+        empty_squares = [square for square in self.__squares if square.value == BoardSquare.empty_value]
+        return random.choice(empty_squares)
 
+    def get_random_value(self):
+        return random.randrange(1,3)
+
+    def set_empty_square(self):
+        random_square = self.pop_random_square()
+        random_square.value = self.get_random_value()
+
+    def move(self, x, y):
+        self.combine_squares(x,y)
+        self.shift_squares(x,y)
+
+  #  def shift_squares(self, x, y):
+       # if(get_square)
+
+    def get_square(self, x, y):
+        return [square for square in self.__squares if square.get_coordinates()['x'] == x and square.get_coordinates()['y'] == y]
