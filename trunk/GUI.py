c98e30ff437ea58f0ab404bd47a15d7fe101e8a5
lejtmany <lejtmany@3eaf17b4-47c3-2875-94e7-d4e5b387a752>
2015-01-21 05:19:44 +0000

    worked on gui grid of labels displays value of square

--- trunk/GUI.py
+++ trunk/GUI.py
@@ -1,10 +1,24 @@
 __author__ = 'Binary_Ninja'
 
 from tkinter import *
+import Board
 
 class GUI(Frame):
 
-    def __int__(self, master = None):
-        Frame.__init__(self, master)
-        self.pack()
+    def __init__(self, master = NONE):
+        Frame.__init__(self,master)
+        Label(self, text="hello there")
+        board = Board.Board()
+        for r in range(board.height):
+            for c in range(board.width):
+                Label(self, text = board.get_square(c,r).value, bd=2, relief=RAISED).grid(row=r, column=c, sticky=N+S+E+W)
+                self.columnconfigure(c,weight=1)
+                self.rowconfigure(r,weight=1)
+
+if __name__ == '__main__':
+    root = Tk()
+    gui = GUI(root).grid(sticky=N+S+E+W)
+    root.columnconfigure(0, weight=1)
+    root.mainloop()
+
 
