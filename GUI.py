a1be7a09778c03e22196fec05205b1b12c454f7c
lejtmany <lejtmany@3eaf17b4-47c3-2875-94e7-d4e5b387a752>
2015-01-22 05:05:38 +0000

    completed key bindings in gui

--- GUI.py
+++ GUI.py
@@ -8,16 +8,29 @@ class GUI(Frame):
     def __init__(self, master = NONE):
         Frame.__init__(self,master)
         Label(self, text="hello there")
-        board = Board.Board()
-        for r in range(board.height):
-            for c in range(board.width):
-                Label(self, text = board.get_square(c,r).value, bd=2, relief=RAISED).grid(row=r, column=c, sticky=N+S+E+W)
+        self.board = Board.Board()
+        self.__labels = {}
+        for r in range(self.board.height):
+            for c in range(self.board.width):
+                Label(self, text = self.board.get_square(c,r).value, bd=2, relief=RAISED).grid(row=r, column=c, sticky=N+S+E+W)
                 self.columnconfigure(c,weight=1)
                 self.rowconfigure(r,weight=1)
 
+    def move_board(self,x,y):
+        self.board.move(x,y)
+
+#def move_board(gui,x,y):
+ #   gui.move_board(x,y)
+
+
 if __name__ == '__main__':
     root = Tk()
-    gui = GUI(root).grid(sticky=N+S+E+W)
+    gui = GUI(root)
+    gui.grid(sticky=N+S+E+W)
+    root.bind("<Up>", lambda event: gui.move_board(0,1))
+    root.bind("<Down>", lambda event: gui.move_board(0,-1))
+    root.bind("<Right>", lambda event: gui.move_board(1,0))
+    root.bind("<Left>", lambda event: gui.move_board(-1,0))
     root.columnconfigure(0, weight=1)
     root.mainloop()
 
