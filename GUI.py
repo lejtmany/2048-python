c3c062cdfb33027dcf024763600ca2c153076eb6
lejtmany <lejtmany@3eaf17b4-47c3-2875-94e7-d4e5b387a752>
2015-01-23 04:09:51 +0000

    labels now update in gui fixed bugs in board runs but doesnt shift right

--- GUI.py
+++ GUI.py
@@ -7,21 +7,29 @@ class GUI(Frame):
 
     def __init__(self, master = NONE):
         Frame.__init__(self,master)
-        Label(self, text="hello there")
         self.board = Board.Board()
-        self.__labels = {}
+        self.__string_vars = self.__generate_stringvars()
         for r in range(self.board.height):
             for c in range(self.board.width):
-                Label(self, text = self.board.get_square(c,r).value, bd=2, relief=RAISED).grid(row=r, column=c, sticky=N+S+E+W)
+                Label(self, textvariable = self.__string_vars[self.board.get_square(c,r)], bd=2, relief=RAISED).grid(row=r, column=c, sticky=N+S+E+W)
                 self.columnconfigure(c,weight=1)
                 self.rowconfigure(r,weight=1)
 
     def move_board(self,x,y):
         self.board.move(x,y)
+        self.__update_string_vars()
 
-#def move_board(gui,x,y):
- #   gui.move_board(x,y)
+    def __generate_stringvars(self):
+        string_vars = {}
+        for square in self.board.get_square_list():
+                board_str_var =  StringVar()
+                string_vars[square] = board_str_var
+                board_str_var.set(str(square.value))
+        return string_vars
 
+    def __update_string_vars(self):
+        for square in self.board.get_square_list():
+            self.__string_vars[square].set(square.value)
 
 if __name__ == '__main__':
     root = Tk()
