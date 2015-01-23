8c0aa4a3df8864a8a554623104f871357957e7e0
lejtmany <lejtmany@3eaf17b4-47c3-2875-94e7-d4e5b387a752>
2015-01-23 05:20:15 +0000

    shift inda works, not really

--- BoardSquare.py
+++ BoardSquare.py
@@ -16,3 +16,6 @@ class BoardSquare:
     def set_coordinates(self, x, y):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return "x:" + str(self.x) + " y:" + str(self.y) + " value:" + str(self.value)
