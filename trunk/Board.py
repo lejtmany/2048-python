31f4d4ee2edd7559c3b977fe4512e68fec68fc00
lejtmany <lejtmany@3eaf17b4-47c3-2875-94e7-d4e5b387a752>
2015-01-23 04:09:51 +0000

    labels now update in gui fixed bugs in board runs but doesnt shift right

--- trunk/Board.py
+++ trunk/Board.py
@@ -6,14 +6,16 @@ class Board:
     width = 4
 
     def __init__(self):
-        self.__generate_board_squares()
+        self.__squares = self.__generate_board_squares()
+        self.__set_empty_square()
         self.game_over = False
 
     def __generate_board_squares(self):
-        self.__squares = []
+        squares = []
         for x in range(0,4):
             for y in range(0,4):
-                self.__squares.append(BoardSquare(x, y, BoardSquare.empty_value))
+                squares.append(BoardSquare(x, y, BoardSquare.empty_value))
+        return squares
 
     def __get_random_square(self):
         empty_squares = [square for square in self.__squares if square.value == BoardSquare.empty_value]
@@ -29,26 +31,31 @@ class Board:
     def move(self, deltaX, deltaY):
         self.__check_game_over()
         self.__shift_squares(deltaX,deltaY)
-        self.combine_squares(deltaX,deltaY)
+        self.__combine_squares(deltaX,deltaY)
         self.__set_empty_square()
 
     def __shift_squares(self, deltaX, deltaY):
         for square in self.__squares:
             adjacent_square = self.__get_adjacent_square(square, deltaX, deltaY)
-            while adjacent_square.value == -1:
-                square.set_coordinates(adjacent_square.get_coordinates())
+            if adjacent_square == None: continue
+            while adjacent_square.value == BoardSquare.empty_value:
+                adjacent_square.value, square.value = square.value, BoardSquare.empty_value
+                adjacent_square = self.__get_adjacent_square(adjacent_square, deltaX, deltaY)
+                if adjacent_square == None: break
 
     def __combine_squares(self, deltaX, deltaY):
-        for square in self.__squares:
+        non_empty_squares = [square for square in self.__squares if square != BoardSquare.empty_value]
+        for square in non_empty_squares:
             adjacent_square = self.__get_adjacent_square(square, deltaX, deltaY)
+            if adjacent_square == None or adjacent_square.value == BoardSquare.empty_value: continue
             while adjacent_square.value == square.value:
-                square.value = -1
+                square.value = BoardSquare.empty_value
                 adjacent_square.value *= 2
 
-    def __get_adjacent_square(square,deltaX, deltaY, self):
+    def __get_adjacent_square(self,square,deltaX, deltaY):
         coordinates = square.get_coordinates()
         adjacent_coordinates = coordinates['x'] + deltaX, coordinates['y'] + deltaY
-        return self.get_square(adjacent_coordinates['x'], adjacent_coordinates['y'])
+        return self.get_square(adjacent_coordinates[0], adjacent_coordinates[1])
 
     def __check_game_over(self):
         if [square for square in self.__squares if square.value == -1] == []:
@@ -58,4 +65,9 @@ class Board:
 
 
     def get_square(self, x, y):
-        return [square for square in self.__squares if square.get_coordinates()['x'] == x and square.get_coordinates()['y'] == y][0]
+        matching = [square for square in self.__squares if square.get_coordinates()['x'] == x and square.get_coordinates()['y'] == y]
+        if matching: return matching[0]
+        else: return None
+
+    def get_square_list(self):
+        return self.__squares
